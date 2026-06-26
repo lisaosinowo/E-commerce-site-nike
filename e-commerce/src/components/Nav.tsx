f5109825df16ea85a21d8bd948ae8e191e4f455f
lisaosinowo1 <lisa.osinowo@capgemini.com>
2026-06-26 14:01:06 +0100

    Added functionality to the mobile burger menu that toggles the navbar visability

--- e-commerce/src/components/Nav.tsx
+++ e-commerce/src/components/Nav.tsx
@@ -1,10 +1,14 @@
 import NikeLogo from "../assets/nike-logo.svg";
 import { RxHamburgerMenu } from "react-icons/rx";
-import {TbShoppingBag} from "react-icons/tb";
+import { TbShoppingBag } from "react-icons/tb";
+import {useState} from "react";
 
 const ROUTES: string[] = ["Home", "About", "Services", "Pricing", "Contact"];
 
 const Nav = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav className="flex flex-wrap justify-between items-center">
       <a href="#">
@@ -12,12 +16,12 @@ const Nav = () => {
         <img src={NikeLogo} className="h-20 w-20" alt="Nike Logo" />
       </a>
       {/* Burger Menu */}
-      <button className="lg:hidden hover:bg-gray-100 focus:ring-2 focus:ring-gray-200 p-2 rounded-lg">
+      <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="lg:hidden hover:bg-gray-100 focus:ring-2 focus:ring-gray-200 p-2 rounded-lg cursor-pointer">
         <RxHamburgerMenu size={25} />
       </button>
 
       {/* Navigation Links */}
-      <div className="w-full lg:w-auto"> {/* Full width on small screens, auto width on large screens */}
+      <div className={`${isMenuOpen ? "" : "hidden"} w-full lg:w-auto lg:block`}> {/* Full width on small screens, auto width on large screens */}
         <ul className="lg:space-x-8 flex flex-col lg:flex-row lg:bg-transparent lg:border-none bg-gray-50 text-lg border border-gray-100 rounded-lg p-4">
           {/*Loop through each item in the ROUTES array and create a list item for each */}
           {ROUTES.map((route, i) => <li className={`px-3 py-2 cursor-pointer rounded ${i === 0 ? "bg-purple-400 text-white lg:bg-transparent lg:text-purple-400" : "hover:bg-gray-100"}`} key={route}>{route}</li>)} {/*A unique key is required for each list item */}
