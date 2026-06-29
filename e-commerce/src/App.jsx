76277d07f68c4493a8ec7f1b8cb8f96915adb588
lisaosinowo1 <lisa.osinowo@capgemini.com>
2026-06-29 17:43:11 +0100

    Created a card component for the different nike shoes

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,11 +1,14 @@
 import Nav from "./components/Nav";
 import ShoeDetail from "./components/ShoeDetail";
+import Card from "./components/Card";
+import { SHOES } from "./constant";
 
 const App = () => {
   return (
     <div className="p-10 xl: px-10 animate-fadeIn">
-          <Nav />
-          <ShoeDetail />
+          {/* <Nav />
+          <ShoeDetail /> */}
+      <Card item={SHOES[0]} />
     </div>
   );
 }
---
+++ e-commerce/src/components/Card.tsx
@@ -0,0 +1,26 @@
+type Item = {
+  id: number;
+  src: string;
+  className: string;
+  title: string;
+  description: string;
+  price: number;
+};
+
+const Card = ({ item }: { item: Item }) => {
+  return (
+    <div
+      className={`${item.className} relative max-w-xl transform transition hover:scale-105 cursor-pointer`}
+    >
+      <div className="p-8">
+        <div className="text-2xl font-bold">{item.title}</div>
+        <div className="underline underline-offset-4 font-semibold mt-10">
+          SHOP NOW +
+        </div>
+      </div>
+      <img className="absolute top-5 left-[40%] h-40 w-56" src={item.src} />
+    </div>
+  );
+};
+
+export default Card;
